53f685caaca079a5c782eecbc1fcf90e4e339f60
Frankincense <frankincensewelsey@gmail.com>
2025-03-19 21:30:09 +0300

    building the simple liker app

--- main.js
+++ main.js
@@ -3,13 +3,36 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const hearts = document.querySelectorAll(".like-glyph")
+console.log(hearts)
+for (let heart of hearts){
+  heart.addEventListener('click',                                                                                                                                    (e)=>{
+    mimicServerCall()
+    .then((res)=> {
+      setTimeout(alert(res), 1000)
+      if (e.target.textContent === EMPTY_HEART){
+        e.target.textContent = FULL_HEART
+        e.target.className = "activated-heart"
+      }
+      else{
+        e.target.textContent = EMPTY_HEART
+        e.target.classList.remove("activated-heart")
+      }
+    })
+    .catch((err)=>{
+      document.querySelector("#modal").classList.remove("hidden")
+      const p = document.querySelector("#modal-message")
+      p.textContent = err.message
+      setTimeout(()=>document.querySelector("#modal").className = "hidden", 3000)
+    }) 
+    
+  })
+}
 
 
-
-
-//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------                                                                  
 // Don't change the code below: this function mocks the server response
-//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------                                                                                                                                                                                                                                                                                                                                                                                            
 
 function mimicServerCall(url="http://mimicServer.example.com", config={}) {
   return new Promise(function(resolve, reject) {
